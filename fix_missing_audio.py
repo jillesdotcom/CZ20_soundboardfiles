8debfcc1c9bbfaa1f648b6bd8850ce8ecc57b82a
Jilles Groenendijk <jilles@jilles.com>
2020-07-21 11:08:52 +0200

    Fix missing TTS audio glitch

---
+++ fix_missing_audio.py
@@ -0,0 +1,32 @@
+# Author: Jilles Groenendijk
+# 
+# Purpose:
+#   Fix TTS glitch by creating dummy first and generate TTS audio later
+#
+# Version:
+# 1.00 - Initial release
+#
+import os, wifi, ugTTS
+
+wifi.connect()
+
+if not wifi.wait():
+	print("No wifi")
+else:
+	for appname in os.listdir("/apps"):
+		filename="/cache/appnames/"+appname+".mp3"
+		print("Checking filename: "+filename)
+		if not(os.path.isfile(filename)):
+			print("creating file: "+filename)
+			fh = open(filename, "wb")
+			fh.close
+	for appname in os.listdir("/apps"):
+		filename="/cache/appnames/"+appname+".mp3"
+		print("Checking filename: "+filename)
+		if os.path.isfile(filename):
+			fh=open(filename, "rb")
+			filesize=len(fh.read())
+			fh.close()
+			if(filesize==0):
+				print("Dummy file:"+filename+" creating TTS")
+				ugTTS.text_to_mp3(appname, filename)
